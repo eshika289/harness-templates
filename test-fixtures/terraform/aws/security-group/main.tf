a4c423b88d186588d2b775cb4ab57aeda31f78fb
Eshika Malgari <102436075+eshika289@users.noreply.github.com>
2026-04-12 18:02:19 -0400

    Create main.tf

---
+++ test-fixtures/terraform/aws/security-group/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Create Security Group in that VPC
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
